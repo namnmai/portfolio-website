6c38a19347a5cbe9002fbe107e440f3fab22046c
Nam Mai <nam.nhmai@gmail.com>
2024-09-17 19:49:06 -0700

    Update settings

--- portfoliowebsite/settings.py
+++ portfoliowebsite/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vtw_en&-+6z_zgio-(pu*@vnzl&fe&1tzaqm%fnguosfio)309'
+# SECRET_KEY = 'django-insecure-vtw_en&-+6z_zgio-(pu*@vnzl&fe&1tzaqm%fnguosfio)309'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
