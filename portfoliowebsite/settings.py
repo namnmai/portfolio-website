0358d1a88861a2132e7d10b498a8aa3c6acb654c
Nam Mai <nam.nhmai@gmail.com>
2024-09-19 20:42:21 -0700

    Update Heroku Postgresql

--- portfoliowebsite/settings.py
+++ portfoliowebsite/settings.py
@@ -89,11 +89,15 @@ WSGI_APPLICATION = 'portfoliowebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# Database configuration
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3')
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
+
 
 
 
