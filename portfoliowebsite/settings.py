72eb2acb49bb9d904f2b7da44cad15e0f359a15b
Nam Mai <nam.nhmai@gmail.com>
2024-09-19 18:46:07 -0700

    Fix database settings

--- portfoliowebsite/settings.py
+++ portfoliowebsite/settings.py
@@ -88,7 +88,10 @@ WSGI_APPLICATION = 'portfoliowebsite.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
